2cb73bd87aaf989c755c95ecc9944f476ddd7948
Kevin <kevindubon43@gmail.com>
2023-11-07 11:30:56 -0800

    Coin flip working as intended, no flip animation.

---
+++ script.js
@@ -0,0 +1,33 @@
+const container = document.querySelector('.container');
+const flipBtn = document.getElementById('flipper');
+const heads = document.createElement('img');
+heads.src = 'svgs/heads.svg';
+heads.setAttribute('id', 'heads');
+const tails = document.createElement('img');
+tails.src = 'svgs/tails.svg';
+tails.setAttribute('id', 'tails');
+
+
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+flipBtn.addEventListener('click', () => {
+    let flipped =  getRandomArbitrary(1, 2);
+    if(flipped == 1) {
+        container.appendChild(heads);
+        if(container.contains(tails)) {
+            container.removeChild(tails);
+        };
+    }
+    else {
+        container.appendChild(tails);
+        if(container.contains(heads)) {
+            container.removeChild(heads);
+        }
+    }
+    container.classList.add('containerShaking');
+    setTimeout(function() {
+        container.classList.remove('containerShaking');
+    }, 250);
+})
